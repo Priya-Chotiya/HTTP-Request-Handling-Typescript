f4f60404a36e8592b6dcb0c9b1d43f0d38c449e2
Chotiya Priya <49747628+Priya-Chotiya@users.noreply.github.com>
2022-10-28 16:45:40 +0530

    Update

--- index.ts
+++ index.ts
@@ -1,36 +1,37 @@
 import fetch from 'node-fetch';
 
-type info = {
+type Info = {
   count: number;
   pages: number;
   next: string;
   prev:number;
 };
 
-type result = {
+type Result = {
   id: number;
   name: string;
   air_date: string;
   episode:string;
-  characters:character[];
+  characters:Character[];
   url:string;
   created:string
 };
 
-type origin = {
+type Origin = {
 name:string;
 location:string;
 }
 
-type character = {
+type Character = {
+  json(): any;
   id: number;
   name: string;
   status:string;
   species:string;
   type:string;
   gender:string;
-  origin:origin;
-  location:origin;
+  origin:Origin;
+  location:Origin;
   image:string;
   episode:string[];
   url:string;
@@ -38,8 +39,8 @@ type character = {
 };
 
 type GetEpisodesResponse = {
-  info: info;
-  results:result[];
+  info: Info;
+  results:Result[];
 };
 
 async function getAllEpisodes() {
@@ -70,6 +71,29 @@ async function getAllEpisodes() {
     }
   }
 }
+async function getAllCharacters(characters:string[]) {
+  try {
+    let res = await Promise.all(characters.map((e:string) => fetch(e , {
+      method: 'GET',
+      headers: {
+        Accept: 'application/json',
+      },
+    })));
+    let resJson = await Promise.all(res.map((e:any) => e.json())) ;
+    return resJson;
+  } catch (err) {
+    console.log(err);
+  }
+}
 
- 
-getAllEpisodes();
+
+getAllEpisodes().then(async (res:any) => {
+  let list = res.results;
+
+  for (let i = 0; i < list.length; i++) {
+    let characters = await getAllCharacters(list[i].characters);
+    Object.assign(list[i], { characters });
+  }
+
+  console.log("Final Result With Update Character Array >>>", {info:res.info ,results:list });
+});
